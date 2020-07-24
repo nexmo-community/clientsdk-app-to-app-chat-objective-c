ee8bfde7b82444de2eb1f0fe5a6059278cfe9c3a
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2020-07-24 11:31:01 +0100

    Initial Commit

---
+++ AppToAppChat/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AppToAppChat
+//
+//  Created by Abdulhakim Ajetunmobi on 24/07/2020.
+//  Copyright © 2020 Vonage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
